2c89b6af0047e05a0c00bfad9cfba0629dd475fe
shinkufencer <infomation.high.macross@gmail.com>
2021-10-10 16:34:11 +0900

    [MODIFY] add relation user -> post
    * Postから該当ユーザの情報が引けるように修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  belongs_to :user, foreign_key: :owner_id
 end
--- db/migrate/20211009103106_create_posts.rb
+++ db/migrate/20211009103106_create_posts.rb
@@ -2,11 +2,13 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.bigint :owner_id
-      t.string :title
-      t.text :homepage_url
-      t.text :message
+      t.string :title, null:false
+      t.text :homepage_url, null:false
+      t.text :message, null:false
 
       t.timestamps
     end
+
+    add_index :posts, :owner_id
   end
 end
