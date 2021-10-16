cd998805daa60a33d752cacbbd2add2c76d1a855
shinkufencer <infomation.high.macross@gmail.com>
2021-10-16 14:30:16 +0900

    [MODIFY] not raise why un make user record
    * ユーザーレコードがない状態でページに来るとActiveRecordNotfoundがraiseされて404が成されてしまうので、回避策としてfind_byに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # @return [User] セッション情報に連動するUserオブジェクトを返却する。DBからのSelectを複数回行いたくないのでインスタンス変数化
   def session_user
-    @memorize_session_user ||= User.find(session[:user_id])
+    @memorize_session_user ||= User.find_by_id(session[:user_id])
   end
 
   def logged_in?
