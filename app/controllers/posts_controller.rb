2341a3e8b04152dca6dbe215268907dad55d047f
shinkufencer <infomation.high.macross@gmail.com>
2021-10-17 17:36:13 +0900

    [MODIFY] un use before_action for instance var
    * インスタンス変数のセットをすべてbefore_actionをやめる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
-  before_action :set_new_arrival_posts, only: %i[ show edit ]
-  before_action :set_relation_posts,:set_pickup_post, only: %i[ show ]
-  before_action :set_recommend_posts, only: %i[ show edit]
-
   def index
     @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @new_arrival_posts = Post.new_arrival_posts(@post)
+    @relation_posts = Post.relation_posts(@post)
+    pickup_post = Post.pickup_post
+    @recommend_posts = Post.new_arrival_posts(pickup_post)
   end
 
   def new
@@ -16,6 +16,10 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @new_arrival_posts = Post.new_arrival_posts(@post)
+    @relation_posts = Post.relation_posts(@post)
+    @recommend_posts = Post.new_arrival_posts(nil)
   end
 
   def create
@@ -33,6 +37,7 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: "Post was successfully updated." }
@@ -45,7 +50,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post.destroy
+    post = Post.find(params[:id])
+    post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
@@ -53,29 +59,6 @@ class PostsController < ApplicationController
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    def set_new_arrival_posts
-      @new_arrival_posts = Post.new_arrival_posts(@post)
-    end
-
-    # @return [ActiveRecord::Relation<Post>] 閲覧中の投稿に関連する投稿
-    def set_relation_posts
-      @relation_posts = Post.relation_posts(@post)
-    end
-
-    # @return [Post] ピックアップしたい投稿
-    def set_pickup_post
-      @pickup_post = Post.pickup_post
-    end
-
-    # @return [ActiveRecord::Relation<Post>] 状況を加味したおすすめ投稿一覧
-    def set_recommend_posts
-      # おすすめはピックアップを除外した新着ポスト
-      @recommend_posts = Post.new_arrival_posts(@pickup_post)
-    end
 
     def post_params
       params.require(:post).permit(:owner_id, :title, :homepage_url, :message)
